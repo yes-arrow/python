c09af828d0b873da5a4bc8f45da48a3a46f7acfe
yes-arrow <60501760+yes-arrow@users.noreply.github.com>
2020-01-31 13:25:36 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,13 @@
+print("this is my first repositry for python")
+
+endy1="Y"
+endy2="E"
+endy3="S"
+endy5="A"
+endy6="R"
+endy7="R"
+endy8="O"
+endy9="W"
+
+print(endy1+endy2+endy3,end="-")
+print(endy5+endy6+endy7+endy8+endy9)
